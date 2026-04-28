c81701ec4b6475ddfb606167b79120b94f1738fb
Andy Dolan <andy.dolan@mmlfm.com>
2026-04-28 11:54:51 +0100

    add initial dotnet web app

---
+++ MMLHub.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MMLHub.Web.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
