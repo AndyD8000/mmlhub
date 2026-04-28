54c45a4fc62df8afd507b88d1a95db43d5d2577f
Andy Dolan <andy.dolan@mmlfm.com>
2026-04-28 17:03:26 +0100

    Add work order details page

--- MMLHub.Web/Services/IWorkOrderService.cs
+++ MMLHub.Web/Services/IWorkOrderService.cs
@@ -5,6 +5,7 @@ namespace MMLHub.Web.Services;
 public interface IWorkOrderService
 {
     Task<List<WorkOrder>> GetWorkOrdersAsync();
+
+    Task<WorkOrder?> GetWorkOrderByIdAsync(int id);
 }
 
-Task<WorkOrder?> GetWorkOrderByIdAsync(int id);
