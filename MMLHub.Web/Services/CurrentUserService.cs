7b9c1fddd7dc5f898a419fc3fdb801728505bc8d
Andy Dolan <andy.dolan@mmlfm.com>
2026-04-28 19:56:11 +0100

    Add role-aware navigation

--- MMLHub.Web/Services/CurrentUserService.cs
+++ MMLHub.Web/Services/CurrentUserService.cs
@@ -3,7 +3,8 @@ namespace MMLHub.Web.Services;
 public class CurrentUserService
 {
     public string UserName => "Test User";
-    public string Role => "ClientUser"; // change to "MMLAdmin" to test admin mode
+    //public string Role => "ClientUser"; // change to "MMLAdmin" to test admin mode
+    public string Role => "MMLAdmin";
     public int ClientId => 1;
 
     public bool IsAdmin => Role == "MMLAdmin";
