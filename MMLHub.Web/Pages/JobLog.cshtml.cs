8d770599bf79d7f9d438ce79b6ee5a3728d49f5f
Andy Dolan <andy.dolan@mmlfm.com>
2026-04-28 12:19:00 +0100

    Add MML Hub layout shell

---
+++ MMLHub.Web/Pages/JobLog.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MMLHub.Web.Pages;
+
+public class JobLogModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
---
+++ MMLHub.Web/Pages/WorkOrders.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MMLHub.Web.Pages;
+
+public class WorkOrdersModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
